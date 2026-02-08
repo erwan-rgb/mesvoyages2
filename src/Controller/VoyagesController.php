90b7026e669eb3a28aefa5461ca378e4b21f37bb
Erwan Menez Lacome <erw.menlac@gmail.com>
2026-02-08 18:16:40 +0100

    Phase 4 : plusieurs pages

---
+++ src/Controller/VoyagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of VoyagesController
+ *
+ * @author erwme
+ */
+class VoyagesController extends AbstractController{
+    
+   #[Route('/voyages', name: 'voyages')]
+    public function index(): Response{
+        return $this->render("pages/voyages.html.twig");
+    }
+}
