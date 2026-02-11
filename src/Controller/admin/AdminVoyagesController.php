f4236584b843b337d9e395e3678448c181ed68fc
Erwan Menez Lacome <erw.menlac@gmail.com>
2026-02-11 11:16:55 +0100

    Phase 12 : ORM (formulaire pour modifier une visite).

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -11,6 +11,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\VisiteRepository;
+use App\Form\VisiteType;
+use Symfony\Component\HttpFoundation\Request;
+
 
 /**
  * Description of AdminVoyagesController
@@ -45,9 +48,23 @@ class AdminVoyagesController extends AbstractController{
     public function suppr(int $id): Response 
     { $visite = $this->repository->find($id);
       $this->repository->remove($visite);
-      return $this->redirectToRoute('admin.voyages');
-         
+      return $this->redirectToRoute('admin.voyages');   
+    }
     
+    #[Route('/admin/edit/{id}', name: 'admin.voyage.edit')]
+    public function edit(int $id, Request $request): Response 
+    { $visite = $this->repository->find($id);
+      $formVisite = $this->createForm(VisiteType::class, $visite);
+      
+      $formVisite->handleRequest($request);
+      if($formVisite->isSubmitted() && $formVisite->isValid()){
+          $this->repository->add($visite);
+          return $this->redirectToRoute('admin.voyages');
+      }
+      return $this->render('admin/admin.voyage.edit.html.twig', [
+          'visite' => $visite,
+          'formvisite' => $formVisite->createView()  
+      ]);   
     }
     
     
