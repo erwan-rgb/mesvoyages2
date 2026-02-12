ed39b05c17fdfdf6e0fb663101c24ff3d5a780e9
Erwan Menez Lacome <erw.menlac@gmail.com>
2026-02-12 12:27:43 +0100

    Phase 18 : gestion des images

---
+++ migrations/Version20260212091927.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260212091927 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE visite_environnement ADD CONSTRAINT FK_6690F746C1C5DC59 FOREIGN KEY (visite_id) REFERENCES visite (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE visite_environnement ADD CONSTRAINT FK_6690F746BAFB82A1 FOREIGN KEY (environnement_id) REFERENCES environnement (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE visite_environnement DROP FOREIGN KEY FK_6690F746C1C5DC59');
+        $this->addSql('ALTER TABLE visite_environnement DROP FOREIGN KEY FK_6690F746BAFB82A1');
+    }
+}
--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -7,14 +7,51 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
 
 #[ORM\Entity(repositoryClass: VisiteRepository::class)]
+#[Vich\Uploadable]
 class Visite
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+    
+    #[Vich\UploadableField(mapping: 'visites', fileNameProperty: 'imageName', size: 'imageSize')]
+    private ?File $imageFile = null;
+    
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+     
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
+      
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+    
+    #[Assert\Callback] 
+    public function validate(ExecutionContextInterface $context){
+        $file = $this->getImageFile();
+        if($file != null && $file != ""){
+            $poids = @filesize($file);
+            if($poids != false && $poids > 512000){
+                $context->buildViolation("Cette image est trop lourde (500ko max)")
+                ->atPath('imageFile')
+                ->addViolation();
+            }
+            $infosImage = @getimagesize($file);
+            if($infosImage == false){
+                $context->buildViolation("Ce fichier n'est pas une image")
+                        ->atPath('imageFile')
+                        ->addViolation();
+            }
+        }
+    }
 
     #[ORM\Column(length: 50)]
     private ?string $ville = null;
@@ -170,5 +207,33 @@ class Visite
 
          return $this;
      }
-    
+ 
+     public function getImageFile(): ?File {
+         return $this->imageFile;
+     }
+
+     public function getImageName(): ?string {
+         return $this->imageName;
+     }
+
+     public function getImageSize(): ?int {
+         return $this->imageSize;
+     }
+
+     public function setImageFile(?File $imageFile): void {
+         $this->imageFile = $imageFile;
+         if(null !== $imageFile){
+             $this->updatedAt = new \DateTimeImmutable();
+         }
+     }
+
+     public function setImageName(?string $imageName): void {
+         $this->imageName = $imageName;
+     }
+
+     public function setImageSize(?int $imageSize): void {
+         $this->imageSize = $imageSize;
+     }
+
+
 }
